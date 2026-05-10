b381ed918f25bf231eeb917a098345718757646b
ensarcan371-boop <ensarcan371@gmail.com>
2026-05-10 12:59:23 +0300

    Add src/main/java/com/yampej/module/modules/misc/MiddleClickFriend.java

---
+++ src/main/java/com/yampej/module/modules/misc/MiddleClickFriend.java
@@ -0,0 +1,10 @@
+package com.yampej.module.modules.misc;
+
+import com.yampej.module.Category;
+import com.yampej.module.Module;
+
+public class MiddleClickFriend extends Module {
+    public MiddleClickFriend() {
+        super("MiddleClickFriend", "Add players as friend", Category.MISC);
+    }
+}

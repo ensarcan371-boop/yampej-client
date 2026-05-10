5a5dab919f6aab63f4bbfd746e1526d39aecf2a9
ensarcan371-boop <ensarcan371@gmail.com>
2026-05-10 12:59:22 +0300

    Add src/main/java/com/yampej/module/modules/misc/DiscordRPC.java

---
+++ src/main/java/com/yampej/module/modules/misc/DiscordRPC.java
@@ -0,0 +1,11 @@
+package com.yampej.module.modules.misc;
+
+import com.yampej.module.Category;
+import com.yampej.module.Module;
+
+public class DiscordRPC extends Module {
+    public DiscordRPC() {
+        super("DiscordRPC", "Show activity in Discord", Category.MISC);
+        setEnabled(true);
+    }
+}

8dc6a702148bee742ba8801cf8b1f6d7c37d1c0c
ensarcan371-boop <ensarcan371@gmail.com>
2026-05-10 12:59:26 +0300

    Add src/main/java/com/yampej/module/modules/misc/PacketCanceller.java

---
+++ src/main/java/com/yampej/module/modules/misc/PacketCanceller.java
@@ -0,0 +1,10 @@
+package com.yampej.module.modules.misc;
+
+import com.yampej.module.Category;
+import com.yampej.module.Module;
+
+public class PacketCanceller extends Module {
+    public PacketCanceller() {
+        super("PacketCanceller", "Cancel certain packets", Category.MISC);
+    }
+}

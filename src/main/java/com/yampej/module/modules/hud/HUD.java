73ae5d81152c406cde4649284c116b51ddfd4247
ensarcan371-boop <ensarcan371@gmail.com>
2026-05-10 12:59:27 +0300

    Add src/main/java/com/yampej/module/modules/hud/HUD.java

---
+++ src/main/java/com/yampej/module/modules/hud/HUD.java
@@ -0,0 +1,11 @@
+package com.yampej.module.modules.hud;
+
+import com.yampej.module.Category;
+import com.yampej.module.Module;
+
+public class HUD extends Module {
+    public HUD() {
+        super("HUD", "Main HUD overlay", Category.HUD);
+        setEnabled(true);
+    }
+}
